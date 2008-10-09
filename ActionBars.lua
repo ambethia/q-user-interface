38e667330c9ee8e0676fd4cb50cff020df1fe60f
Jason L Perry <jasper@ambethia.com>
2008-10-09 16:52:41 -0400

    Chat, and some other crap

---
+++ ActionBars.lua
@@ -0,0 +1,8 @@
+local f = CreateFrame("frame")
+f:RegisterEvent("PLAYER_LOGIN")
+f:SetScript("OnEvent", function()
+  f:UnregisterAllEvents()
+  f:SetScript("OnEvent", nil)
+  
+  MainMenuBar:Hide() -- Hide Blizzard Bar
+end)
--- oUF_Layout.lua
+++ oUF_Layout.lua
@@ -1,20 +1,20 @@
-local texture      = [[Interface\AddOns\Q\Media\Minimalist]]
-local calibri      = [[Interface\AddOns\Q\Media\Calibri.ttf]]
-local calibri_bold = [[Interface\AddOns\Q\Media\CalibriBold.ttf]]
+local STATUS_BAR = [[Interface\AddOns\Q\Media\Minimalist]]
+local FONT       = [[Interface\AddOns\Q\Media\Calibri.ttf]]
+local FONT_B     = [[Interface\AddOns\Q\Media\CalibriBold.ttf]]
 
-local spacing,          baseline        = 5,  (UIParent:GetHeight() - (UIParent:GetHeight() * 0.618))
-local primary_height,   primary_width   = 26, (((UIParent:GetWidth()/2) * 0.618) - spacing)
-local secondary_height, secondary_width = 10, ((primary_width * 0.618) - spacing)
+local SPACING = 5
+local PRIMARY_HEIGHT,   PRIMARY_WIDTH   = 26, (((UIParent:GetWidth()/2) * 0.618) - SPACING)
+local SECONDARY_HEIGHT, SECONDARY_WIDTH = 10, ((PRIMARY_WIDTH * 0.618) - SPACING)
 
 local menu = function(self)
-	local unit = self.unit:sub(1, -2)
-	local cunit = self.unit:gsub("(.)", string.upper, 1)
+  local unit = self.unit:sub(1, -2)
+  local cunit = self.unit:gsub("(.)", string.upper, 1)
 
-	if(unit == "party" or unit == "partypet") then
-		ToggleDropDownMenu(1, nil, _G["PartyMemberFrame"..self.id.."DropDown"], "cursor", 0, 0)
-	elseif(_G[cunit.."FrameDropDown"]) then
-		ToggleDropDownMenu(1, nil, _G[cunit.."FrameDropDown"], "cursor", 0, 0)
-	end
+  if(unit == "party" or unit == "partypet") then
+    ToggleDropDownMenu(1, nil, _G["PartyMemberFrame"..self.id.."DropDown"], "cursor", 0, 0)
+  elseif(_G[cunit.."FrameDropDown"]) then
+    ToggleDropDownMenu(1, nil, _G[cunit.."FrameDropDown"], "cursor", 0, 0)
+  end
 end
 
 local updateHealth = function(self, event, unit, bar, min, max)
@@ -61,9 +61,9 @@ local function ambethianStyle(settings, self, unit)
   self:SetAttribute("*type2", "menu")
 
   if (unit == "focus") or (unit == "targettarget") then
-    width = secondary_width
+    width = SECONDARY_WIDTH
   else -- player or target
-    width = primary_width
+    width = PRIMARY_WIDTH
   end
 
   self.TaggedStrings = {}
@@ -71,21 +71,21 @@ local function ambethianStyle(settings, self, unit)
   -- Health Bar
   local health = CreateFrame("StatusBar")
   health:SetWidth(width)
-  health:SetHeight(primary_height-1)
-  health:SetStatusBarTexture(texture)
+  health:SetHeight(PRIMARY_HEIGHT-1)
+  health:SetStatusBarTexture(STATUS_BAR)
 
   health:SetParent(self)
-  health:SetPoint("TOP", 1, 1)
+  health:SetPoint("TOP", self, "BOTTOM", 0, (SECONDARY_HEIGHT + PRIMARY_HEIGHT))
 
   -- Health Bar Background
   local healthBG = health:CreateTexture(nil, "BORDER")
   healthBG:SetAllPoints()
-  healthBG:SetTexture(texture)
+  healthBG:SetTexture(STATUS_BAR)
   
   -- Health HP Text
   local healthHP = health:CreateFontString(nil, "OVERLAY", "GameFontNormalSmall")
-  healthHP:SetFont(calibri, 10)
-  healthHP:SetPoint("BOTTOMLEFT", 1, 1)
+  healthHP:SetFont(FONT, 9)
+  healthHP:SetPoint("BOTTOMLEFT", 2, 2)
   healthHP:SetJustifyH("LEFT")
   healthHP:SetTextColor(1, 1, 1, 0.66)
 
@@ -96,22 +96,22 @@ local function ambethianStyle(settings, self, unit)
   self.OverrideUpdateHealth = updateHealth
 
   -- Unit Name
-  local name = health:CreateFontString(nil, "OVERLAY", "GameFontHighlight")
-  name:SetFont(calibri_bold, 13)
-  name:SetPoint("TOPLEFT")
+  local name = health:CreateFontString(nil, "OVERLAY", "GameFontNormal")
+  name:SetFont(FONT_B, 11)
+  name:SetPoint("TOPLEFT", 2, -2)
   name:SetJustifyH("LEFT")
   name:SetTextColor(1, 1, 1, 0.66)
   name:SetText("[name]")
   table.insert(self.TaggedStrings, name)
 
   -- Info
-	local info = health:CreateFontString(nil, "OVERLAY", "GameFontNormal")
-	info:SetFont(calibri, 13)
-	info:SetPoint("TOPRIGHT")
-	info:SetJustifyH("RIGHT")
-	info:SetTextColor(1, 1, 1)
-	info:SetText("[difficulty][smartlevel] [rare] [raidcolor][smartclass]")
-	table.insert(self.TaggedStrings, info)
+  local info = health:CreateFontString(nil, "OVERLAY", "GameFontNormal")
+  info:SetFont(FONT, 11)
+  info:SetPoint("TOPRIGHT", -2, -2)
+  info:SetJustifyH("RIGHT")
+  info:SetTextColor(1, 1, 1)
+  info:SetText("[difficulty][smartlevel] [rare] [raidcolor][smartclass]")
+  table.insert(self.TaggedStrings, info)
 
   self.Info = info
   self.UNIT_LEVEL = updateInfoString
@@ -120,15 +120,15 @@ local function ambethianStyle(settings, self, unit)
   -- Power
   local power = CreateFrame("StatusBar")
   power:SetWidth(width)
-  power:SetHeight(secondary_height)
-  power:SetStatusBarTexture(texture)
+  power:SetHeight(SECONDARY_HEIGHT)
+  power:SetStatusBarTexture(STATUS_BAR)
 
   power:SetParent(self)
-  power:SetPoint("TOPLEFT", health, "BOTTOMLEFT", 0, -1)
+  power:SetPoint("BOTTOM", self, "BOTTOM", 0, 0)
 
   local powerBg = power:CreateTexture(nil, "BORDER")
   powerBg:SetAllPoints()
-  powerBg:SetTexture(texture)
+  powerBg:SetTexture(STATUS_BAR)
   powerBg.multiplier = 0.33
   power.bg = powerBg
 
@@ -153,9 +153,9 @@ local function ambethianStyle(settings, self, unit)
   -- Cast Bar
   if (unit == "player" or unit == "target" or unit == "focus") then
     local cast = CreateFrame("StatusBar")
-    cast:SetWidth(width - (secondary_height*2) - 2)
-    cast:SetHeight(secondary_height*2)
-    cast:SetStatusBarTexture(texture)
+    cast:SetWidth(width - (SECONDARY_HEIGHT*2) - 2)
+    cast:SetHeight(SECONDARY_HEIGHT*2)
+    cast:SetStatusBarTexture(STATUS_BAR)
     cast:SetStatusBarColor(.8, .8, .8)
     cast:SetParent(self)
     cast:SetFrameStrata("MEDIUM")
@@ -163,28 +163,28 @@ local function ambethianStyle(settings, self, unit)
   
     local castBg = cast:CreateTexture(nil, "BORDER")
     castBg:SetAllPoints()
-    castBg:SetTexture(texture)
+    castBg:SetTexture(STATUS_BAR)
     castBg:SetVertexColor(.3, .3, .3)
     cast.bg = castBg
 
     local castTime = cast:CreateFontString(nil, "OVERLAY", "GameFontNormalSmall")
-    castTime:SetFont(calibri, 10)
-    castTime:SetPoint("RIGHT", cast, -spacing, 0)
+    castTime:SetFont(FONT, 10)
+    castTime:SetPoint("RIGHT", cast, -SPACING, 0)
     castTime:SetTextColor(1, 1, 1, 0.66)
     cast.Time = castTime
 
     local castText = cast:CreateFontString(nil, "OVERLAY", "GameFontNormalSmall")
-    castText:SetFont(calibri, 10)
-    castText:SetPoint("LEFT", cast, spacing, 0)
-    castText:SetPoint("RIGHT", castTime, "LEFT", -spacing, 0)
+    castText:SetFont(FONT, 10)
+    castText:SetPoint("LEFT", cast, SPACING, 0)
+    castText:SetPoint("RIGHT", castTime, "LEFT", -SPACING, 0)
     castText:SetJustifyH("LEFT")
     castText:SetTextColor(1, 1, 1, 0.66)
     cast.Text = castText
 
     local castIcon = cast:CreateTexture(nil, "BORDER")
     castIcon:SetPoint("RIGHT", cast, "LEFT", -2, 0)
-    castIcon:SetWidth(secondary_height*2)
-    castIcon:SetHeight(secondary_height*2)
+    castIcon:SetWidth(SECONDARY_HEIGHT*2)
+    castIcon:SetHeight(SECONDARY_HEIGHT*2)
     cast.Icon = castIcon
 
     self.Castbar = cast
@@ -192,27 +192,27 @@ local function ambethianStyle(settings, self, unit)
 end
 
 oUF:RegisterStyle("Q - Primary", setmetatable({
-  ["initial-width"] = primary_width,
-  ["initial-height"] = primary_height + (secondary_height*3),
+  ["initial-width"] = PRIMARY_WIDTH,
+  ["initial-height"] = PRIMARY_HEIGHT + (SECONDARY_HEIGHT*3),
 }, {__call = ambethianStyle}))
 
 oUF:RegisterStyle("Q - Secondary", setmetatable({
-  ["initial-width"] = secondary_width,
-  ["initial-height"] = primary_height + (secondary_height*3),
+  ["initial-width"] = SECONDARY_WIDTH,
+  ["initial-height"] = PRIMARY_HEIGHT + (SECONDARY_HEIGHT*3),
 }, {__call = ambethianStyle}))
 
 oUF:SetActiveStyle("Q - Primary")
 
 local player = oUF:Spawn("player", "oUF_Player")
-player:SetPoint("TOPRIGHT", UIParent, "BOTTOM", -spacing/2, baseline)
+player:SetPoint("BOTTOMRIGHT", WorldFrame, "BOTTOM", -SPACING/2, SPACING)
 
 local target = oUF:Spawn("target", "oUF_Target")
-target:SetPoint("TOPLEFT", UIParent, "BOTTOM", spacing/2, baseline)
+target:SetPoint("BOTTOMLEFT", WorldFrame, "BOTTOM", SPACING/2, SPACING)
 
 oUF:SetActiveStyle("Q - Secondary")
 
 local targettarget = oUF:Spawn("targettarget", "oUF_TargetTarget")
-targettarget:SetPoint("TOPLEFT", target, "TOPRIGHT", spacing, 0)
+targettarget:SetPoint("TOPLEFT", target, "TOPRIGHT", SPACING, 0)
 
 local focus = oUF:Spawn("focus", "oUF_Focus")
-focus:SetPoint("TOPRIGHT", player, "TOPLEFT", -spacing, 0)
+focus:SetPoint("TOPRIGHT", player, "TOPLEFT", -SPACING, 0)
